699a104a14dbcbf8930f4247de62bbe788e7f809
girish p kumar <girish.kumar1@tothenew.com>
2022-01-13 03:13:02 +0530

    download script 1

--- statuscheck.sh
+++ statuscheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while read LINE; do   curl -o /dev/null --silent --get --write-out "%{http_code} $LINE\n" "$LINE"; done < all-urls.txt | grep 'en' > file1.txt 
+while read LINE; do   curl -o /dev/null --silent --get --write-out "%{http_code} $LINE\n" "$LINE"; done < all-urls.txt | grep '200' > down.txt 
