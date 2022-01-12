d63bbf317bb13f8bc10fd220fd7a750f19d7c177
girish p kumar <girish.kumar1@tothenew.com>
2022-01-13 03:44:54 +0530

    download script 3 update

--- statuscheck.sh
+++ statuscheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while read LINE; do   curl -o /dev/null --silent --get --write-out "%{http_code} $LINE\n" "$LINE"; done < all-urls.txt > all_url_list.txt
 
-cat full-list.txt | grep "en" > error_url_list.txt
+cat all_url_list.txt | grep "en" > error_url_list.txt
