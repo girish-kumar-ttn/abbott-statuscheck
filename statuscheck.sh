21606d62a125b5a85b8c955bd5237e9466c14315
girish p kumar <girish.kumar1@tothenew.com>
2022-01-17 15:19:02 +0530

    filter added

--- statuscheck.sh
+++ statuscheck.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 while read LINE; do   curl -o /dev/null --silent --get --write-out "%{http_code} $LINE\n" "$LINE"; done < all-urls.txt > all_url_list.txt
 
-cat all_url_list.txt | grep "en" > error_url_list.txt
+
+while read line; do
+j=$(echo "$line" | awk '{print $1}')
+if [[ "$j" != "200" ]]
+then
+echo "$line" > error_url_list.txt
+fi
+done < all_url_list.txt
